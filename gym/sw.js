2b7f5a4e910676e93ba8fbf9d7c2ef92e020b7dc
flychico <51941733+flychico@users.noreply.github.com>
2026-08-24 18:18:40 -0400

    Add files via upload

--- gym/sw.js
+++ gym/sw.js
@@ -1,5 +1,5 @@
 /* Michelangelo offline cache */
-const C = "mangelo-v12";
+const C = "mangelo-v13";
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(["/gym/", "/gym/index.html", "/gym/manifest.json"])));
   self.skipWaiting();
